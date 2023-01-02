937c3aa86280b5646fce992944b0df9b87f791f4
Caio Cabral <ocaiocabral@gmail.com>
2023-01-02 17:32:27 -0400

    Reorganizing project

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let transformRate = -99.99;
 
 buttonsList.forEach(button => {
   button.addEventListener("click", (e) => {    
+    e.target.disabled = true;
     let direction = e.target.dataset.direction;
     if(direction==="next"){
       swiper.style.transition = "transform 2s ease-in-out";
@@ -16,8 +17,6 @@ buttonsList.forEach(button => {
           swiper.style.transition = "transform 0s ease-in-out";
           transformRate = -33.33;
           swiper.style.transform = `translateX(${transformRate}vw)`;}, 2000);
-/*         swiper.style.transition = "none";
-        swiper.style.transition = "transform 2s ease-in-out"; */
       }
     } else if(direction === "previous"){
       swiper.style.transition = "transform 2s ease-in-out";
@@ -29,10 +28,9 @@ buttonsList.forEach(button => {
           swiper.style.transition = "transform 0s ease-in-out";
           transformRate = -166.65;
           swiper.style.transform = `translateX(${transformRate}vw)`;}, 2000);
-/*         swiper.style.transition = "none";
-        swiper.style.transition = "transform 2s ease-in-out"; */
       }
     }
+    setTimeout(() => e.target.disabled = false, 2000);
   })
 });
 
--- readme.md
+++ readme.md
@@ -25,18 +25,18 @@ Working on responsivity
 Access: https://marcelluscaio.github.io/pagina-taty-fotografia/
 
 ### Still to come
-[ ] Tematicas no swiper
-[ ] Header sticks a while
-[ ] aumeentar botao swiper
-[ ] criar secao home
-[ ] Jogar sobre mim para se;'ao sobre mim
-[ ]  Criar secao portfolio
-[ ] Na secao de portfolio cta agende seu ensaio
-[ ] clicando na foto, mostra ela ( como)
-[ ] Estrutura: swiper CTA Mosaico
+[ ] Estrutura: Swiper CTA Mosaico
 [ ] Responsivity
-[ ] Add new photos
+[ ] Aumentar botao swiper
 [ ] Control Grid height
-[ ] Improve footer (use film strips?)
+[ ] Header sticks a while
+[ ] Criar secao home
+[ ] Jogar sobre mim para secao sobre mim
+[ ] Criar secao portfolio
+[ ] Na secao de portfolio cta agende seu ensaio
+[ ] Clicando na foto, mostra ela (como)
+[ ] Add new photos
 [ ] Portfolio for each theme 
 [ ] Parcerias
+[ ] Tematicas no swiper
+[ ] Improve footer (use film strips?)
